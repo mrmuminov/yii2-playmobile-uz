4afc525a2251499880f284c3a5aee229a180e073
Bahriddin Mo'minov <darkshadeuz@gmail.com>
2021-03-25 22:12:48 +0500

    Change readme, fix response content, create param getters and setters and some fixes

--- Playmobile.php
+++ Playmobile.php
@@ -3,22 +3,46 @@
 namespace mrmuminov\yii2playmobileuz;
 
 use yii\base\Component;
+use yii\base\InvalidConfigException;
 use yii\httpclient\Client;
+use yii\httpclient\Exception;
+use yii\httpclient\Request;
 
 /**
  * This is just an example.
  */
 class Playmobile extends Component
 {
+    /**
+     * @var string $baseUrl
+     */
     public $baseUrl = "http://91.204.239.44/broker-api/";
 
+    /**
+     * @var string $username
+     */
     private $username;
 
+    /**
+     * @var string $password
+     */
     private $password;
 
+    /**
+     * @var Client $client
+     */
     private $client;
+
+    /**
+     * @var Request $request
+     */
     private $request;
 
+    /**
+     * Playmobile constructor.
+     * @param $username
+     * @param $password
+     */
     public function __construct($username, $password)
     {
         $this->username = $username;
@@ -26,7 +50,94 @@ class Playmobile extends Component
         parent::__construct();
     }
 
-    public function create($options){
+
+    /**
+     * @return string
+     */
+    public function getBaseUrl(): string
+    {
+        return $this->baseUrl;
+    }
+
+    /**
+     * @param string $baseUrl
+     */
+    public function setBaseUrl(string $baseUrl): void
+    {
+        $this->baseUrl = $baseUrl;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param string $username
+     */
+    public function setUsername(string $username): void
+    {
+        $this->username = $username;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param string $password
+     */
+    public function setPassword(string $password): void
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * @return Client
+     */
+    public function getClient(): Client
+    {
+        return $this->client;
+    }
+
+    /**
+     * @param Client $client
+     */
+    public function setClient(Client $client): void
+    {
+        $this->client = $client;
+    }
+
+    /**
+     * @return Request
+     */
+    public function getRequest(): Request
+    {
+        return $this->request;
+    }
+
+    /**
+     * @param Request $request
+     */
+    public function setRequest(Request $request): void
+    {
+        $this->request = $request;
+    }
+
+
+    /**
+     * @param array $options
+     * @throws InvalidConfigException
+     */
+    public function create($options = [])
+    {
         if (isset($options['baseUrl'])) {
             $this->baseUrl = $options['baseUrl'];
         }
@@ -34,6 +145,11 @@ class Playmobile extends Component
         $this->request = $this->client->createRequest();
     }
 
+    /**
+     * @param array $messages
+     * @return false|string
+     * @throws Exception
+     */
     public function send($messages = [])
     {
         $data = [];
@@ -52,7 +168,6 @@ class Playmobile extends Component
             }
         }
 
-
         $response = $this->request
             ->setUrl("send")
             ->setMethod("POST")
@@ -63,7 +178,7 @@ class Playmobile extends Component
             ])
             ->send();
         if ($response->isOk) {
-            return $response->data;
+            return $response->content;
         }
         return false;
     }
--- README.md
+++ README.md
@@ -10,7 +10,11 @@ The preferred way to install this extension is through [composer](http://getcomp
 Either run
 
 ```
-php composer.phar require --prefer-dist mrmuminov/yii2-playmobile-uz "*"
+php composer.phar require mrmuminov/yii2-playmobile-uz "*"
+```
+or
+```
+composer require mrmuminov/yii2-playmobile-uz
 ```
 
 or add
@@ -28,4 +32,22 @@ Usage
 Once the extension is installed, simply use it in your code by  :
 
 ```php
-<?= \mrmuminov\yii2playmobileuz\AutoloadExample::widget(); ?>```
+
+use mrmuminov\yii2playmobileuz\Playmobile;
+
+$playmobile = new Playmobile('username', 'password');
+
+$playmobile->create([
+    'baseUrl' => "http://91.204.239.44/broker-api/"
+]);
+
+$response = $playmobile->send([
+        [
+            'recipient' => "998936913932",
+            'message-id' => "1",
+            'originator' => "3700",
+            'text' => "test",
+        ],
+]);
+echo $response == false ? "Not sent" : "Send successfully";
+```
